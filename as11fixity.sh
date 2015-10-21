2ba1043ac2432ac04f55e21a5720a35bca42eec6
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-10-21 23:23:34 +0100

    Update as11fixity.sh

--- as11fixity.sh
+++ as11fixity.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
-#not functioning, just a proof of concept to test out loops
-#find "$1" -name "*.xml" | (
-#	while read file; do
+find "$1" -name "*.xml" | (
+	while read file; do
 
+	sourcepath="$(dirname "$file")" 
+	filename="$(basename "$file")"
+	filenoext="${filename%.*}"
+	xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$sourcepath/${filenoext}.xml"
+	md5 "$sourcepath/${filenoext}.mxf"
 
-#	md5 "$file"
-#	xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$file"
-	
-
-#done
-#)
+done
+)
