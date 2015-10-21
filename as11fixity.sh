70dce602c091aa86707039c386399e503e917eb8
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-10-21 23:52:27 +0100

    added basic if statement

--- as11fixity.sh
+++ as11fixity.sh
@@ -1,12 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash 
+#http://stackoverflow.com/a/15930450/2188572
+#only look for xml files in all directories below your chosen directory
 find "$1" -name "*.xml" | (
 	while read file; do
 
 	sourcepath="$(dirname "$file")" 
 	filename="$(basename "$file")"
 	filenoext="${filename%.*}"
-	xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$sourcepath/${filenoext}.xml"
-	md5 "$sourcepath/${filenoext}.mxf"
-
+    md5xml=($(xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$sourcepath/${filenoext}.xml"))
+	md5mxf=($(md5deep "$sourcepath/${filenoext}.mxf"))
+	if [[ "${md5xml}" == "${md5mxf}" ]] ; then
+		echo "all is well!"
+	else 
+		echo "something is wrong!"
+	fi
 done
 )
