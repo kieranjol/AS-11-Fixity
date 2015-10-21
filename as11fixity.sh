504a65c1ce37a1e8849b1efe7b75ec216b469ed5
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-10-21 22:37:11 +0100

    Create as11fixity.sh

---
+++ as11fixity.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+#not functioning, just a proof of concept to test out loops
+#find "$1" -name "*.xml" | (
+#	while read file; do
+
+
+#	md5 "$file"
+#	xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$file"
+	
+
+#done
+#)
