70b536195ff9f90cd3da3dad7c8a77e148cf561e
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-10-22 10:15:37 +0100

    added processing status update

--- as11fixity.sh
+++ as11fixity.sh
@@ -3,11 +3,12 @@
 #only look for xml files in all directories below your chosen directory
 find "$1" -name "*.xml" | (
 	while read file; do
-
+	
 	sourcepath="$(dirname "$file")" 
 	filename="$(basename "$file")"
 	filenoext="${filename%.*}"
-    md5xml=($(xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$sourcepath/${filenoext}.xml"))
+	echo "Processing "$file"
+	md5xml=($(xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$sourcepath/${filenoext}.xml"))
 	md5mxf=($(md5deep "$sourcepath/${filenoext}.mxf"))
 	if [[ "${md5xml}" == "${md5mxf}" ]] ; then
 		echo "all is well!"
