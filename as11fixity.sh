22f18bd54aed25704fd1671860b9535eb5621b57
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-16 14:47:03 +0000

    Cleanup

--- as11fixity.sh
+++ as11fixity.sh
@@ -7,26 +7,26 @@ if [ -f "$1".csv ]; then
 else
 echo "Filename,Title,Episode_Number,Md5_From_Xml,Md5_from_Mxf,Checksum_Result" >> "$1".csv 
 find "$1" -name "*.xml" | (
-	while IFS= read -r file; do
+while IFS= read -r file; do
 
-	sourcepath="$(dirname "$file")" 
-	filename="$(basename "$file")"
-	filenoext="${filename%.*}"
-	echo "Processing "$file""
+    sourcepath="$(dirname "$file")" 
+    filename="$(basename "$file")"
+    filenoext="${filename%.*}"
+    echo "Processing "$file""
     md5xml=($(xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Technical/x:Additional/x:MediaChecksumValue" "$sourcepath/${filenoext}.xml"))
     title=($(xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Editorial/x:ProgrammeTitle" "$sourcepath/${filenoext}.xml"))
     epnum=($(xml sel -N 'x=http://www.digitalproductionpartnership.co.uk/ns/as11/2013' -t -v "//x:Programme/x:Editorial/x:EpisodeTitleNumber" "$sourcepath/${filenoext}.xml"))
 	
-	md5mxf=($(md5deep -e "$sourcepath/${filenoext}.mxf"))
-	if [[ "${md5xml}" == "" ]] ; then
-             echo "not a sidecar"
-        elif [[ "${md5xml}" == "${md5mxf}" ]]  ; then
-	    echo "all is well!"
-	    echo ""$file","$title","$epnum","$md5xml","$md5mxf", Correct Checksum" >> "$1".csv 
-	else 
-	    echo "something is wrong!"
-	    echo ""$file","$title","$epnum","$md5xml","$md5mxf",Bad Checksum" >> "$1".csv 
-	fi
+    md5mxf=($(md5deep -e "$sourcepath/${filenoext}.mxf"))
+    if [[ "${md5xml}" == "" ]] ; then
+        echo "not a sidecar"
+    elif [[ "${md5xml}" == "${md5mxf}" ]]  ; then
+        echo "all is well!"
+	echo ""$file","$title","$epnum","$md5xml","$md5mxf", Correct Checksum" >> "$1".csv 
+    else 
+	echo "something is wrong!"
+	echo ""$file","$title","$epnum","$md5xml","$md5mxf",Bad Checksum" >> "$1".csv 
+    fi
 	
 done
 )
